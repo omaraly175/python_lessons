535d51eccc7bdfaed2196e13abcbd2c456bdd008
omaraly175 <omaraly175@proton.me>
2022-12-26 12:19:57 -0500

    prime factorization

---
+++ prime_factorization.py
@@ -0,0 +1,45 @@
+def sieve(a):
+    primes = []
+    l = [*range(2, a + 1)]
+    while len(l) > 0:
+        for i in l:
+            if i != l[0] and i % l[0] == 0:
+                l.remove(i)
+        primes.append(l.pop(0))
+
+    return primes
+
+def isprime(y):
+    z = sieve(y)
+    return y in z
+    
+
+def multiples(a):
+    divisors = []
+    b = int(a / 2)
+    c = b + 1
+    for h in range(c):
+        if h != 0 and a % h == 0:
+            divisors.append(h)
+    divisors.append(a)
+    return(divisors)
+
+def factorization(a):
+    factors = []
+    l = sieve(a)
+    for i in l:
+        while a % i == 0:
+            a = a / i
+            factors.append(i)
+    return(factors)
+
+    
+    
+
+a = input("Give me a number, and I will tell you if it is prime or not and its prime factorization.\r\n")
+a = int(a)
+
+if isprime(a):
+    print("This number is prime. Its divisors are only", multiples(a))
+else:
+    print("This number is not prime. Its divisors are", multiples(a), "and its prime factorization is", factorization(a))
